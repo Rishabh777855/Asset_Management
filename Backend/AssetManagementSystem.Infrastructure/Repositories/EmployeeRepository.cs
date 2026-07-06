10ca49ecf77fd277cf52e803f14a9de7f2115b06
LT10026\Shrey Shah <shshah@tenupsoft.com>
2026-07-06 10:53:43 +0530

    feat: add employee repo

--- Backend/AssetManagementSystem.Infrastructure/Repositories/EmployeeRepository.cs
+++ Backend/AssetManagementSystem.Infrastructure/Repositories/EmployeeRepository.cs
@@ -1,1 +1,28 @@
+using AssetManagementSystem.Domain.Entities;
+using AssetManagementSystem.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace AssetManagementSystem.Infrastructure.Repositories;
+
+public class EmployeeRepository(ApplicationDbContext context): Repository<Employee>(context), IEmployeeRepository
+{
+     public async Task<Employee?> GetByEmailAsync(string email)
+    {
+        return await context.Employees
+            .FirstOrDefaultAsync(e => e.Email == email);
+    }
+
+    public async Task<Employee?> GetEmployeeWithAssetsAsync(Guid employeeId)
+    {
+        return await context.Employees
+            .Include(e => e.AssetAssignments)
+            .ThenInclude(a => a.Asset)
+            .FirstOrDefaultAsync(e => e.Id == employeeId);
+    }
+
+    public async Task<bool> ExistsAsync(Guid employeeId)
+    {
+        return await context.Employees
+            .AnyAsync(e => e.Id == employeeId);
+    }
+}
